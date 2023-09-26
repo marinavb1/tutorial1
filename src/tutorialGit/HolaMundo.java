c6b7c74aadc0d3609592356e71f6271c13539d01
marin <marin@Marina>
2023-09-26 17:05:55 +0200

    Nuevo mensaje

--- src/tutorialGit/HolaMundo.java
+++ src/tutorialGit/HolaMundo.java
@@ -3,8 +3,8 @@ package tutorialGit;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!");
-
+		System.out.println("Hola CMEPPS!");
+		System.out.println("Bienvenidos al curso!");
 	}
 
 }
